4c8cb7c85737dc52bfed8a3d26ad1669799bfe29
hx4u-L <sherapsy@gmail.com>
2025-05-26 12:36:31 -0700

    Update register.py

--- register.py
+++ register.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os.path
 import csv
@@ -31,38 +31,38 @@ requiredField = ['FirstName', 'Surname', 'StreetAddress1', 'Town', 'State', 'Pos
 if __name__ == "__main__":
     # execute only if run as a script
     if not os.path.exists(configFileName):
-        print '"{}" is not found.\nPlease make a copy from "{}"'.format(configFileName, sampleConfigFileName)
+        print('"{}" is not found.\nPlease make a copy from "{}"'.format(configFileName, sampleConfigFileName))
         exit()
 
     config = configparser.ConfigParser()
     config.read(configFileName)
 
     contactInfo = {}
-    for postKey, configKey in configDict.iteritems():
+    for postKey, configKey in configDict.items():
         postValue = config.get(configSection, configKey) if config.has_option(configSection, configKey) else None
-        if requiredField.__contains__(postKey) and (postValue is None or postValue == ''):
-            print "'{}' is missing".format(postKey)
+        if postKey in requiredField and (postValue is None or postValue == ''):
+            print("'{}' is missing".format(postKey))
             exit()
         contactInfo[postKey] = postValue
 
     try:
-        f = open(csvFileName, 'r')
+        f = open(csvFileName, 'r', newline='', encoding='utf-8')
     except (OSError, IOError) as error:
-        print '"{}" is not found.\nPlease make a copy from "{}"'.format(csvFileName, sampleCsvFileName)
+        print('"{}" is not found.\nPlease make a copy from "{}"'.format(csvFileName, sampleCsvFileName))
         exit()
-
 
     titles = ['Last 4', 'Registered']
     separator = '  '
     header = separator.join(titles)
-    print header
-    print '=' * len(header)
+    print(header)
+    print('=' * len(header))
 
     for row in csv.reader(f):
-        if row[0] == 'Card Number': # CSV Header
+        if row[0] == 'Card Number':  # CSV Header
             continue
         vgc = VisaGiftCard.fromRow(row)
         if vgc.valid:
             success = vgc.registerCard(contactInfo)
-            print '{:>6}  {:>10}'.format(vgc.lastFour, 'Yes' if success else 'No')
-    
+            print('{:>6}  {:>10}'.format(vgc.lastFour, 'Yes' if success else 'No'))
+
+    f.close()
